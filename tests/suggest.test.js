b0a083f8f8a85bd548139566e7d27f39e0af2088
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2020-07-23 23:37:17 +0200

    test: Make sure the payload is output on error (#6)

--- tests/suggest.test.js
+++ tests/suggest.test.js
@@ -22,6 +22,77 @@ const { makeReview } = require("../src/GitHubClient");
    }} Review
  */
 
+const FIXTURE_UNIDIFF = [
+  "diff --git a/src/Graphics/TextureAllocator.gl.h b/src/Graphics/TextureAllocator.gl.h",
+  "index 366b30f7..f17e3c88 100644",
+  "--- a/src/Graphics/TextureAllocator.gl.h",
+  "+++ b/src/Graphics/TextureAllocator.gl.h",
+  "@@ -18,8 +18,8 @@ namespace rainbow::graphics::gl",
+  " ",
+  "         void destroy(TextureHandle&) override;",
+  " ",
+  "-        [[maybe_unused, nodiscard]]",
+  "-        auto max_size() const noexcept -> size_t override;",
+  "+        [[maybe_unused, nodiscard]] auto max_size() const noexcept",
+  "+            -> size_t override;",
+  " ",
+  "         void update(const TextureHandle&,",
+  "                     const Image&,",
+  "diff --git a/src/Graphics/VertexArray.h b/src/Graphics/VertexArray.h",
+  "index 31e66c01..8bc6fc35 100644",
+  "--- a/src/Graphics/VertexArray.h",
+  "+++ b/src/Graphics/VertexArray.h",
+  "@@ -50,10 +50,7 @@ namespace rainbow::graphics",
+  "         /// <summary>",
+  "         ///   Returns whether this vertex array object is valid.",
+  "         /// </summary>",
+  "-        explicit operator bool() const",
+  "-        {",
+  "-            return static_cast<bool>(array_);",
+  "-        }",
+  "+        explicit operator bool() const { return static_cast<bool>(array_); }",
+  " ",
+  "     private:",
+  " #ifdef USE_VERTEX_ARRAY_OBJECT",
+].join("\n");
+
+const FIXTURE_UNIDIFF_PAYLOAD = {
+  accept: "application/vnd.github.comfort-fade-preview+json",
+  owner: "tido64",
+  repo: "suggestion-bot",
+  pull_number: 0,
+  event: "COMMENT",
+  comments: [
+    {
+      path: "src/Graphics/TextureAllocator.gl.h",
+      line: 22,
+      side: "RIGHT",
+      start_line: 21,
+      start_side: "RIGHT",
+      body: [
+        "```suggestion",
+        "        [[maybe_unused, nodiscard]] auto max_size() const noexcept",
+        "            -> size_t override;",
+        "```",
+        "",
+      ].join("\n"),
+    },
+    {
+      path: "src/Graphics/VertexArray.h",
+      line: 56,
+      side: "RIGHT",
+      start_line: 53,
+      start_side: "RIGHT",
+      body: [
+        "```suggestion",
+        "        explicit operator bool() const { return static_cast<bool>(array_); }",
+        "```",
+        "",
+      ].join("\n"),
+    },
+  ],
+};
+
 describe("suggest", () => {
   const { GITHUB_EVENT_PATH, GITHUB_REPOSITORY } = process.env;
 
@@ -59,82 +130,31 @@ describe("suggest", () => {
 
   test("supports unified diffs", async () => {
     let payload = undefined;
-    await makeReview(
-      [
-        "diff --git a/src/Graphics/TextureAllocator.gl.h b/src/Graphics/TextureAllocator.gl.h",
-        "index 366b30f7..f17e3c88 100644",
-        "--- a/src/Graphics/TextureAllocator.gl.h",
-        "+++ b/src/Graphics/TextureAllocator.gl.h",
-        "@@ -18,8 +18,8 @@ namespace rainbow::graphics::gl",
-        " ",
-        "         void destroy(TextureHandle&) override;",
-        " ",
-        "-        [[maybe_unused, nodiscard]]",
-        "-        auto max_size() const noexcept -> size_t override;",
-        "+        [[maybe_unused, nodiscard]] auto max_size() const noexcept",
-        "+            -> size_t override;",
-        " ",
-        "         void update(const TextureHandle&,",
-        "                     const Image&,",
-        "diff --git a/src/Graphics/VertexArray.h b/src/Graphics/VertexArray.h",
-        "index 31e66c01..8bc6fc35 100644",
-        "--- a/src/Graphics/VertexArray.h",
-        "+++ b/src/Graphics/VertexArray.h",
-        "@@ -50,10 +50,7 @@ namespace rainbow::graphics",
-        "         /// <summary>",
-        "         ///   Returns whether this vertex array object is valid.",
-        "         /// </summary>",
-        "-        explicit operator bool() const",
-        "-        {",
-        "-            return static_cast<bool>(array_);",
-        "-        }",
-        "+        explicit operator bool() const { return static_cast<bool>(array_); }",
-        " ",
-        "     private:",
-        " #ifdef USE_VERTEX_ARRAY_OBJECT",
-      ].join("\n"),
-      {
-        createReview: (review) => {
-          payload = review;
-          return Promise.resolve();
-        },
-      }
-    );
-    expect(payload).toEqual({
-      accept: "application/vnd.github.comfort-fade-preview+json",
-      owner: "tido64",
-      repo: "suggestion-bot",
-      pull_number: 0,
-      event: "COMMENT",
-      comments: [
-        {
-          path: "src/Graphics/TextureAllocator.gl.h",
-          line: 22,
-          side: "RIGHT",
-          start_line: 21,
-          start_side: "RIGHT",
-          body: [
-            "```suggestion",
-            "        [[maybe_unused, nodiscard]] auto max_size() const noexcept",
-            "            -> size_t override;",
-            "```",
-            "",
-          ].join("\n"),
-        },
-        {
-          path: "src/Graphics/VertexArray.h",
-          line: 56,
-          side: "RIGHT",
-          start_line: 53,
-          start_side: "RIGHT",
-          body: [
-            "```suggestion",
-            "        explicit operator bool() const { return static_cast<bool>(array_); }",
-            "```",
-            "",
-          ].join("\n"),
-        },
-      ],
+    await makeReview(FIXTURE_UNIDIFF, {
+      createReview: (review) => {
+        payload = review;
+        return Promise.resolve();
+      },
     });
+    expect(payload).toEqual(FIXTURE_UNIDIFF_PAYLOAD);
+  });
+
+  test("dumps the payload on failure", async () => {
+    const dirSpy = jest.spyOn(global.console, "dir").mockImplementation();
+    const errorSpy = jest.spyOn(global.console, "error").mockImplementation();
+
+    await makeReview(FIXTURE_UNIDIFF, {
+      createReview: () => {
+        return Promise.reject("test");
+      },
+    });
+
+    expect(errorSpy).toHaveBeenCalledWith("test");
+    expect(dirSpy).toHaveBeenCalledWith(FIXTURE_UNIDIFF_PAYLOAD, undefined, {
+      depth: null,
+    });
+
+    dirSpy.mockRestore();
+    errorSpy.mockRestore();
   });
 });
